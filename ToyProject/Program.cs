5601e19359022d92a8b0d926a260687ec9c3569f
DuckJeans <lemonfox223@gmail.com>
2024-09-07 14:43:15 +0900

    Unity First Commit

---
+++ ToyProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ToyProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
